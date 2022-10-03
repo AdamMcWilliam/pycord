a94cf69ff01be135b0a65e9fc54de8c531a71243
Adam <adammcwilliam@gmail.com>
2022-10-03 12:12:49 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ Run a Discord bot that takes the !gas command and shows the status in an embed +
 # python3 gas_bot.py -s etherscan
 
 #from asyncio.windows_events import NULL
+from __future__ import print_function
 from typing import Tuple
 import logging
 import yaml
@@ -26,6 +27,13 @@ from requests import Request, Session
 import requests
 from google_images_search import GoogleImagesSearch
 import random
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
 
 
 db = TinyDB('db.json')
@@ -186,6 +194,94 @@ def main(source, verbose=False):
             #print(message.channel)
             await message.channel.send(f"{imgURL}")
         await bot.process_commands(message)
+
+    @bot.command(pass_context=True, brief="Get Community map of our group !community {community id}")
+    async def community(ctx):
+        
+        message = ctx.message.content
+        community = message.split("!community ")
+        community = community[1]
+
+
+        # If modifying these scopes, delete the file token.json.
+        SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+        # The ID and range of a sample spreadsheet.
+        SAMPLE_SPREADSHEET_ID = '1a6xZ2LfJAYj9G3lY7VxdGkyS-rqmSYUzjyXonMUGYow'
+        SAMPLE_RANGE_NAME = 'Data!A2:D'
+
+        """Shows basic usage of the Sheets API.
+        Prints values from a sample spreadsheet.
+        """
+        creds = None
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.json'):
+            creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
+
+        try:
+            service = build('sheets', 'v4', credentials=creds)
+
+            # Call the Sheets API
+            sheet = service.spreadsheets()
+            result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                        range=SAMPLE_RANGE_NAME).execute()
+            values = result.get('values', [])
+
+            if not values:
+                print('No data found.')
+                return
+
+            
+            #community = "36"
+            link = "https://wolfland.live/"+community+"#"
+            lastname = ""
+            nextname = ""
+            #print('Name, Major:')
+            list = []
+            for row in values:
+                if row[3] == community:
+                    list.append([row[0],row[2]])
+            print(list)
+
+            for index, row in enumerate(list):
+                if index + 1 < len(list):
+                    nextEle = list[index+1]
+                    nextname = nextEle[0]
+
+                name = row[0]
+                landid = row[1]
+                
+                print("currentName" + name)
+                print("nextName" + nextname)
+                if name == lastname:
+                    #beginbot=7195,7197,7196,7199,7198&Ravonus=7133
+                    link = link +','+landid 
+                else:
+                    link = link + name +'='+landid
+                if name != nextname and nextname !="":
+                    link = link +"&" 
+                lastname = name
+                    #print(row[0])
+                # Print columns A and E, which correspond to indices 0 and 4.
+                #print('%s, %s' % (row[0], row[3]))
+        except HttpError as err:
+            print(err)
+        
+        #link = "complete"
+        await ctx.send(link)
 
 
     @bot.command(pass_context=True, brief="Show meditation Leaderboard")
