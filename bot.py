fce6f2af5605612c93a05c3aac5459f01aa4057a
Adam <adammcwilliam@gmail.com>
2023-12-23 01:23:40 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -384,17 +384,19 @@ def main(source, verbose=False):
 
                 price = tokenPrice(token,config)
                 embed = discord.Embed(title=f"Current {token} price")
-                embed.add_field(name=f"1 {token} = ", value=f"${price}", inline=True) 
+                if not isinstance(price, dict):
+                        if 'base_token_name' not in price:
+                            embed.add_field(name=f"1 {token} = ", value=f"${price}", inline=True) 
                 
                 if(amount!=0):
                     print(amount)
-                    if not isinstance(price, dict):
-                        if 'base_token_name' not in price:
+                    
                             embed.add_field(name=f"{amount} {token} = ", value=f"${float(price)*float(amount)}", inline=True)
                 #if usdprice array is not empty
                 #if price is dict
                 if isinstance(price, dict):
                     if 'base_token_name' in price:
+                        embed.add_field(name=f"1 {token} = ", value=f"${price['price']:.5f}", inline=True) 
                         embed.add_field(name=f"{amount} {token} = ", value=f"${price['price']:.5f}", inline=True)
                         embed.add_field(name=f"5m: ", value=f"{price['percentage_5m']:.2f}%", inline=True)
                         embed.add_field(name=f"1h: ", value=f"{price['percentage_1h']:.2f}%", inline=True)
