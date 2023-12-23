cd31924e9a96441ad220d1fc4bd59cfdf162632d
Adam <adammcwilliam@gmail.com>
2023-12-23 01:24:29 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -390,8 +390,7 @@ def main(source, verbose=False):
                 
                 if(amount!=0):
                     print(amount)
-                    
-                            embed.add_field(name=f"{amount} {token} = ", value=f"${float(price)*float(amount)}", inline=True)
+                    embed.add_field(name=f"{amount} {token} = ", value=f"${float(price)*float(amount)}", inline=True)
                 #if usdprice array is not empty
                 #if price is dict
                 if isinstance(price, dict):
